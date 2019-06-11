d365328f0462ac0a5eaf4ab18910169b3a4d7aa4
Alun Jones <alunacjones@gmail.com>
2019-06-11 22:00:19 +0100

    More tests

--- LSL.Signing.Tests/ObjectSignerTests.cs
+++ LSL.Signing.Tests/ObjectSignerTests.cs
@@ -14,7 +14,7 @@ namespace LSL.Signing.Tests
     public class ObjectSignerTests
     {
         [TestCaseSource(nameof(Tests))]
-        public void Blah3(Action<ObjectSignerBuilder> binarySerialiserBuilder, Action<ObjectSignerBuilder> signerBuilder, Pair validAndInvalidPair)
+        public void RunTests(Action<ObjectSignerBuilder> binarySerialiserBuilder, Action<ObjectSignerBuilder> signerBuilder, Pair validAndInvalidPair, int expectedSignaureLength)
         {
             MessagePackSerializer.SetDefaultResolver(MessagePack.Resolvers.ContractlessStandardResolver.Instance);
 
@@ -26,6 +26,7 @@ namespace LSL.Signing.Tests
 
             var signature = objectSigner.GenerateSignature(validAndInvalidPair.Valid());
 
+            signature.Length.Should().Be(expectedSignaureLength, "the signer shoulld prduce consistently lengthed signaures");
             objectSigner.Verify(
                 validAndInvalidPair.Valid(),
                 signature
@@ -43,6 +44,12 @@ namespace LSL.Signing.Tests
             public string Name {get;set;}
         }
 
+        [Serializable]
+        public class OuterClass {
+            public int Id {get;set;}
+            public Test Inner {get;set;}
+        }
+
         public static IEnumerable Tests {
             get {
                 var secret = Encoding.UTF8.GetBytes("als-test");
@@ -53,26 +60,28 @@ namespace LSL.Signing.Tests
                     Serialiser("Default", cfg => {})
                 } 
                 from signer in new[] {
-                    Signer("HMAC256", cfg => cfg.WithHmac256SignatureProvider(secret)),
-                    Signer("HMAC384", cfg => cfg.WithHmac384SignatureProvider(secret)),
-                    Signer("HMAC512", cfg => cfg.WithHmac512SignatureProvider(secret)),
-                    Signer("Default", cfg => {})
+                    Signer("HMAC256", 32, cfg => cfg.WithHmac256SignatureProvider(secret)),
+                    Signer("HMAC384", 48, cfg => cfg.WithHmac384SignatureProvider(secret)),
+                    Signer("HMAC512", 64, cfg => cfg.WithHmac512SignatureProvider(secret)),
+                    Signer("Default", 32, cfg => {})
                 }
                 from validAndInvalidPair in new Pair[] {
                     new Pair(() => new Test { Id = 12, Name = "Als" }, () => new Test { Id = 13, Name = "Als"}),
-                    new Pair(() => "qwe", () => "qwe2")
+                    new Pair(() => "qwe", () => "qwe2"),
+                    new Pair(() => new OuterClass { Id = 12, Inner = new Test { Id = 13, Name = "Als" }}, () => new OuterClass { Id = 12, Inner = new Test { Id = 14, Name = "Als" }})
                 }
                 let valid = JsonConvert.SerializeObject(validAndInvalidPair.Valid())
                 let invalid = JsonConvert.SerializeObject(validAndInvalidPair.Invalid())
-                select new TestCaseData(binarySerialiser.Serialiser, signer.SignerBuilder, validAndInvalidPair)
+                select new TestCaseData(binarySerialiser.Serialiser, signer.SignerBuilder, validAndInvalidPair, signer.ExpectedSignatureLength)
                     .SetName($"Given a {binarySerialiser.Name} serialiser and a {signer.Name} signer for a valid value of {valid} and invalid value of {invalid}, it should validate both correctly");
             }
         }
 
-        public static SignerInfo Signer(string name, Action<ObjectSignerBuilder> signerBuilder) => new SignerInfo
+        public static SignerInfo Signer(string name, int expectedSignatureLength, Action<ObjectSignerBuilder> signerBuilder) => new SignerInfo
         {
             Name = name,
-            SignerBuilder = signerBuilder
+            SignerBuilder = signerBuilder,
+            ExpectedSignatureLength = expectedSignatureLength
         };
 
         public static SerialiserInfo Serialiser(string name, Action<ObjectSignerBuilder> serialiserBuilder) => new SerialiserInfo
@@ -84,6 +93,7 @@ namespace LSL.Signing.Tests
         public class SignerInfo {
             public string Name {get;set;}
             public Action<ObjectSignerBuilder> SignerBuilder {get;set;}
+            public int ExpectedSignatureLength { get; internal set; }
         }
 
         public class SerialiserInfo {            
