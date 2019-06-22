8b4068dcffb229074a04e1238126209eb0535c60
Alun Jones <alunacjones@gmail.com>
2019-06-22 23:17:16 +0100

    try and add xml documentation

--- LSL.Signing/IObjectSignerFactory.cs
+++ LSL.Signing/IObjectSignerFactory.cs
@@ -2,8 +2,16 @@ using System;
 
 namespace LSL.Signing
 {
+    /// <summary>
+    /// Facilitates the creation of <see cref="IObjectSigner"/> instances
+    /// </summary>
     public interface IObjectSignerFactory
     {
+        /// <summary>
+        /// Builds a new <see cref="IObjectSigner"/> instance with an optional builder
+        /// </summary>
+        /// <param name="builder">A delegate that is provided an ObjectSignerBuilder for configuring the <see cref="IObjectSigner"/> instance</param>
+        /// <returns></returns>
         IObjectSigner Build(Action<ObjectSignerBuilder> builder = null);
     }
 }
--- LSL.Signing/ObjectSignerFactory.cs
+++ LSL.Signing/ObjectSignerFactory.cs
@@ -2,8 +2,10 @@ using System;
 
 namespace LSL.Signing
 {
+    /// <inheritdoc/>
     public class ObjectSignerFactory : IObjectSignerFactory
     {
+        /// <inheritdoc/>
         public IObjectSigner Build(Action<ObjectSignerBuilder> configurator = null)
         {
             var builder = new ObjectSignerBuilder();
